0efe29e73a7257497cc89d57487a32493851d199
Teemu Lindgren <teemujul@metropolia.fi>
2019-04-02 19:53:57 +0300

    Tehtävä 6. Decorator C) kohta

---
+++ Decorator/src/decorator/Decorator.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package decorator;
+
+/**
+ *
+ * @author teemu
+ */
+public abstract class Decorator implements Pizza{
+
+    public Decorator(Pizza PizzaPohja) {
+        this.PizzaPohja = PizzaPohja;
+    }
+    protected Pizza PizzaPohja;
+    
+    @Override
+    public double getHinta() {
+        return this.PizzaPohja.getHinta();
+    }
+
+    @Override
+    public String getSisalto() {
+        return this.PizzaPohja.getSisalto();
+    }
+    
+    
+}
